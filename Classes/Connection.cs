5827fa41ba47fa3f33e6ca32182faf543ac57963
pokapachka <pachkaosokin@yandex.ru>
2024-05-28 10:50:59 +0500

    Разработанные классы общего пользования. Осокин

---
+++ Classes/Connection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace ПР42_Осокин.Classes
+{
+    public class Connection
+    {
+        private static readonly string config = "server=KAKTYC\\SQLEXPRESS;Trusted_Connection=No;DataBase=ShopContent;User=Pepsi;pwd=0000";
+
+        public static SqlConnection OpenConnection()
+        {
+            SqlConnection connection = new SqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+
+        public static SqlDataReader Query(string SQL, out SqlConnection connection)
+        {
+            connection = OpenConnection();
+            return new SqlCommand(SQL, connection).ExecuteReader();
+        }
+
+        public static void CloseConnection(SqlConnection connection)
+        {
+            connection.Close();
+            SqlConnection.ClearPool(connection);
+        }
+    }
+}
