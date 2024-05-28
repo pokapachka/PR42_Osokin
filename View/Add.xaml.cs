f3e4bb95a1b63191370edff19da1fa5b7f415b4b
pokapachka <pachkaosokin@yandex.ru>
2024-05-28 11:18:21 +0500

    Созданная страница добавления и изменения. Осокин

---
+++ View/Add.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ПР42_Осокин.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Add.xaml
+    /// </summary>
+    public partial class Add : Page
+    {
+        public Add(object Context)
+        {
+            InitializeComponent();
+            DataContext = new
+            {
+                item = Context,
+                categorys = new ViewModell.VMCategorys()
+            };
+        }
+    }
+}
