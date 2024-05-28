37742c76836c6aaad464342429c169dc6ccdd34d
pokapachka <pachkaosokin@yandex.ru>
2024-05-28 10:58:49 +0500

    Разработанные контексты данных. Осокин

---
+++ Context/CategorysContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Text;
+using ПР42_Осокин.Classes;
+using ПР42_Осокин.Modell;
+
+namespace ПР42_Осокин.Context
+{
+    public class CategorysContext : Categorys
+    {
+        public static ObservableCollection<CategorysContext> AllCategorys()
+        {
+            ObservableCollection<CategorysContext> allCategorys = new ObservableCollection<CategorysContext>();
+            SqlConnection connection;
+            SqlDataReader dataCategorys = Classes.Connection.Query("SELECT * FROM Categorys", out connection);
+            while (dataCategorys.Read())
+            {
+                allCategorys.Add(new CategorysContext()
+                {
+                    Id = dataCategorys.GetInt32(0),
+                    Name = dataCategorys.GetString(1)
+                });
+            }
+            Classes.Connection.CloseConnection(connection);
+            return allCategorys;
+        }
+    }
+}
